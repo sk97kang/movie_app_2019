ef1adec2681b33653efa0cb6ab473075c7a02f6e
sk97kang <sk97.kang@gmail.com>
2020-06-02 12:40:08 +0900

    Updates

---
+++ precache-manifest.b3495df4996e4991ce8ccff3c1640cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddfc6ea49b52ab5afaa2c840950e109",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "5eee0e6bbb6e91f721ba",
+    "url": "/movie_app_2019/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "c2b13cb1a43739ba71ce",
+    "url": "/movie_app_2019/static/js/2.f51afafd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.f51afafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eee0e6bbb6e91f721ba",
+    "url": "/movie_app_2019/static/js/main.28b554d4.chunk.js"
+  },
+  {
+    "revision": "ed13f68ad2334e5f99e5",
+    "url": "/movie_app_2019/static/js/runtime-main.49cba434.js"
+  }
+]);
